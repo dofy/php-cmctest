072746beb4268f53d79ae7b2275c169e98785f27
dofyyu@gmail.com <dofyyu@gmail.com@c9966117-aa19-6a95-1ac8-b7125140fb5e>
2012-10-18 13:11:25 +0000

    add Message.php

---
+++ models/Message.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Author:  Seven Yu
+ * E-Mail:  dofyyu@gmail.com
+ * Version: 1.0
+ * Update:  2009-3-5
+ */
+class Message extends SevenModule
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this-> = 'message';
+    }
+
+    public function getList($page)
+    {
+        $this->getCount();
+        return $this->getRows("select * from $this->table order by updated desc", $page);
+    }
+
+    public function getMyMessage($uid)
+    {
+        return $this->getAll("select * from $this->table where uid = $uid order by updated desc");
+    }
+
+    public function getMessage($id)
+    {
+        return $this->getOne("select * from $this->table where id=$id");
+    }
+
+    public function addMessage($message)
+    {
+        return $this->insert($message);
+    }
+
+    public function reply($id, $reply)
+    {
+        return $this->update(array('reply'=>$reply), array('id'=>$id));
+    }
+
+    public function delMessage($id)
+    {
+        return $this->delete(array('id' => $id));
+    }
+
+}
+?>
